5b59ddfe2d9d097cb7c9eead2e8f9d5342239593
MUSAVIR REHMAN <musavirrehman4878@gmail.com>
2022-12-12 10:37:25 +0530

    React facts with the function of switching to dark mode and light mode

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+export default function Navbar(props) {
+    return(
+        <nav className={props.darkMode ? "dark":""}>
+            <img 
+                src={require('../images/React-logo.png')}
+                className="nav-icon"    
+            />
+            <h3 className='nav-logo_text'>React Facts</h3>
+
+            <div className='toggler'>
+                <p className='toggler-light'>Light</p>
+                <div
+                    className='toggler-slider'
+                    onClick={props.toggleDarkMode}
+                    >
+                        <div className='toggler-slider-circle'></div>
+
+                </div>
+                <p className='toggler-dark'>Dark</p>
+            </div>
+        </nav>
+    )
+}
